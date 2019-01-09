a82a4efc43e5bb0365a4e2eeeb57e12830e57f05
Ehtasham Sajid <esajid@folio3.com>
2019-01-09 19:14:00 +0500

    change in php head

--- index.php
+++ index.php
@@ -21,6 +21,17 @@ else
     <!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
     <link rel="stylesheet" href="/styles.css" type="text/css">
 </head>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>PHP Application - AWS Elastic Beanstalk</title>
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lobster+Two" type="text/css">
+    <link rel="icon" href="https://awsmedia.s3.amazonaws.com/favicon.ico" type="image/ico" >
+    <link rel="shortcut icon" href="https://awsmedia.s3.amazonaws.com/favicon.ico" type="image/ico" >
+    <!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+    <link rel="stylesheet" href="/styles.css" type="text/css">
+</head>
 <body>
     <section class="congratulations">
         <h1>Congratulations!</h1>
